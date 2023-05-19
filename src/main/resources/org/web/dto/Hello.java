93a30854476b0d0132ad3038f1a6eb27d3c21e05
Bandaru Devendra <bandarudevendra63057@gmail.com>
2023-05-20 00:01:18 +0530

    Example one

---
+++ src/main/resources/org/web/dto/Hello.java
@@ -0,0 +1,7 @@
+package org.web.dto;
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println("This a sample project!!!!!!!!!!!!!");
+}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body bgcolor="#FFC300">
+<h1 align="center">WELCOME TO HOSPITAL APPLICATION</h1>
+<div class="Hello"> </div>
+</body>
+</html>
